511e4d5e72a98662ffb084ad67a83315de62bb40
jreuel64 <long3522@stthomas.edu>
2019-03-02 21:19:17 -0600

    Fixed Table

--- js/home.js
+++ js/home.js
@@ -1,4 +1,34 @@
 function Init()
 {
-	console.log("Initializing")
+	console.log("Initializing");
+	//GetNews(DisplayNews);
+}
+
+function GetNews(callback)
+{
+	console.log("Getting News");
+
+	var news = new XMLHttpRequest();
+
+	news.onreadystatechange = funtion(){
+		if(news.readyState == 4 && news.status == 200)
+		{
+			console.log("Recieved News");
+			callback(news);
+		}
+	};
+
+	var url = "https://jreuel64.github.io/js/recent_news.json";
+
+	news.open("GET", url, true);
+	news.send();
+}
+
+function DisplayNews(news);
+{
+	var newsField = document.getElementById("news");
+	var jsonNews = JSON.parse(news.responseText());
+
+	console.log(newsField);
+	console.log(jsonNews);
 }
